2d38ec8d91ea75b02e23e21219749fe723203aa7
nikolastojanovic9993 <nikolastojanovic9993@gmail.com>
2022-02-01 16:25:36 +0100

    TASK_9 Formatiranje vremena

--- 29__ZAVRSNI__PROJEKAT/ui.js
+++ 29__ZAVRSNI__PROJEKAT/ui.js
@@ -11,6 +11,28 @@ export class ChatUI {
         return this._element;
     }
 
+
+    dateFormat(date) {
+        let godina = date.getFullYear();
+        let mesec = date.getMonth() + 1;
+
+        let dan = date.getDate();
+
+        let sati = date.getHours();
+
+        let minuti = date.getMinutes();
+
+        //Dodavanje 0 ispred jednocifrenih vrednosti
+        mesec = String(mesec).padStart(2, "0");
+        dan = String(dan).padStart(2, "0");
+        sati = String(sati).padStart(2, "0");
+        minuti = String(minuti).padStart(2, "0");
+
+        let datum = `${dan}.${mesec}.${godina}.-${sati}:${minuti}`;
+        return datum;
+    }
+
+
     //Metod 
     templateLI(data) {
         //Izvuci podatke iz prosledjenog dokumenta(data) i zapisati ih u formatu
@@ -20,8 +42,10 @@ export class ChatUI {
         let htmlLi =
             `<li> ${data.username}: ${data.message} 
         <br> 
-        ${data.created_at}
+        ${this.dateFormat(data.created_at.toDate())}
         </li>`
         this.element.innerHTML += htmlLi;
     }
+
+
 }
