b2c37248fe2cbe884a2bd33ef453cbd0b88066c5
nikolastojanovic9993 <nikolastojanovic9993@gmail.com>
2022-02-03 14:45:56 +0100

    TASK_13 Local Storage

--- 29__ZAVRSNI__PROJEKAT/app.js
+++ 29__ZAVRSNI__PROJEKAT/app.js
@@ -12,6 +12,24 @@ let inputUser = document.getElementById("inputUser");
 let chatroom = new Chatroom("general", "Nikola");
 let chatUI = new ChatUI(lista);
 
+// Postavljanje vrednosti u Local Storage
+// localStorage.setItem("nazivPromenljive", 5);
+// localStorage.setItem("nazivPromenljive", 6);
+// localStorage.setItem("nazivPromenljive", "Test string");
+localStorage.setItem("x", 7);
+// localStorage.setItem("y", 10);
+
+// Uzimanje vrednosti iz Local Storage-a
+let z = localStorage.x + localStorage.y;
+console.log(z);
+console.log(localStorage.x);
+if(localStorage.x) {
+    console.log("X postoji");
+}
+else {
+    console.log("X ne postoji");
+}
+
 //Ispis dokumenta iz db u konzoli
 chatroom.getChats(d => {
     console.log(d);
